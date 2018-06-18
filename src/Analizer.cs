acf8ea674ba9a624dc47782a2b80d866e2a97678
Indrik <indrikozver@gmail.com>
2018-06-18 21:47:50 +0300

    Init solution

---
+++ src/Analizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class Analyser
+    {
+        public async Task Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
