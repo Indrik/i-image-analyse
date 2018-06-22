74458468f83fb9cbdff2b56021739262f5151689
Indrik <indrikozver@gmail.com>
2018-06-23 01:04:16 +0300

    check all arguments

--- src/Ia.Application/Program.cs
+++ src/Ia.Application/Program.cs
@@ -8,16 +8,26 @@ namespace Ia.Application
     {
         public static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length != 2)
             {
                 Console.WriteLine("Please enter arguments: <analysis path> and <limit of area for image in percent>.");
+                return;
+            }
+
+            var path = args[0];
+            var strLimit = args[1];
+
+            int limit;
+            if (!int.TryParse(strLimit, out limit))
+            {
+                Console.WriteLine("Second argument must be integer.");
                 return;
             }
             
             var container = DiInstaller.GetServiceContainer();
             var app = container.GetInstance<IAnalyser>();
             
-            app.RunAsync(null, 10).Wait();
+            app.RunAsync(path, limit).Wait();
         }
     }
 }
